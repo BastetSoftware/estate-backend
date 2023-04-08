2c16857b9f7e8734a32ca78572d75317e96d66d1
teackot <teackot@gmail.com>
2023-04-08 21:38:36 +0300

    Remove unused Listen() function and function numbers.

--- api/api.go
+++ api/api.go
@@ -1,31 +1,15 @@
 package api
 
-import (
-	"fmt"
-	"github.com/vmihailenco/msgpack/v5"
-	"log"
-	"net"
-	"os"
-	"os/signal"
-	"syscall"
-)
-
-//goland:noinspection GoIrregularIota
 const (
-	FPing uint8 = iota
+	EExists uint8 = iota + 1 // record exists
+	ENoEntry
+	EPassWrong
+	ENotLoggedIn
+	EAccessDenied
 
-	FUserCreate
-	FLogIn
-	FLogOut
-	FUserInfo
-	FUserEdit
-	FUserSetManagesGroups
-
-	FGroupCreate
-	FGroupRemove
-	FGroupAddRemoveUser
-
-	FNull uint = iota
+	EArgsInval uint8 = 253 // invalid arguments
+	ENoFun     uint8 = 254 // function does not exist
+	EUnknown   uint8 = 255 // unknown error
 )
 
 type Request struct {
@@ -116,81 +100,4 @@ type ArgsFGroupAddRemoveUser struct {
 	Action bool // true - add, false - remove
 }
 
-const (
-	EExists uint8 = iota + 1 // record exists
-	ENoEntry
-	EPassWrong
-	ENotLoggedIn
-	EAccessDenied
-
-	EArgsInval uint8 = 253 // invalid arguments
-	ENoFun     uint8 = 254 // function does not exist
-	EUnknown   uint8 = 255 // unknown error
-)
-
-// TODO: remove conn (?)
 type RequestHandler func(r []byte) (interface{}, error)
-
-func Listen(address string, handler RequestHandler) error {
-	socket, err := net.Listen("tcp4", address)
-	if err != nil {
-		return fmt.Errorf("unable to create a socket: %v", err)
-	}
-
-	channel := make(chan os.Signal, 1)
-	signal.Notify(channel, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-channel
-		// err := os.Remove(address)
-		if err != nil {
-			log.Fatalf("unable to remove the socket: %v", err)
-		}
-
-		os.Exit(1)
-	}()
-
-	for {
-		conn, err := socket.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		go func(conn net.Conn) {
-			defer conn.Close()
-
-			// read the request
-			buf := make([]byte, 4096)
-			n, err := conn.Read(buf)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// deserialize the request
-			var msg Request
-			err = msgpack.Unmarshal(buf[:n], &msg)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// handle the request
-			//response, err := handler(&msg)
-			if err != nil {
-				log.Println(err)
-			}
-
-			// serialize the response
-			//data, err := msgpack.Marshal(response)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			// send the response
-			//_, err = conn.Write(data)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}(conn)
-	}
-
-	return nil
-}
